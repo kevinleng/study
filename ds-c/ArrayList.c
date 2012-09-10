922bec7a016364ac7d5e347a6cfa4755f3dd1b16
kevin <kevin@kevin-laptop.(none)>
2012-09-10 21:55:53 +0800

    counter version0.1

---
+++ ds-c/ArrayList.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+
+#define MAX_SIZE 50
+
+typedef char ElemType;
+
+typedef struct {
+	ElemType data[MAX_SIZE];
+	int length;
+} ArrayList;
+
+void init(ArrayList *&L){
+	
+}
+
+void destroy(ArrayList *&L){
+
+}
+
+int empty(ArrayList *L){
+
+}
+
+int length(ArrayList *L){
+
+}
+
+void display(ArrayList *L){
+
+}
+
+int getElem(ArrayList *L, int i, ElemType &e){
+
+}
+
+int locateElem(ArrayList *L, ElemType e){
+
+}
+
+int insert(ArrayList *&L, int i, ElemType e){
+
+}
+
+int deleteElem(ArrayList *&L, int i, ElemType &e){
+
+}
+
+
